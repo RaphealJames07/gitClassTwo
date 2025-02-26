ca387ef6190407e0c82d8e10c5d348a90038831e
Stking19 <okolistephen43@gmail.com>
2025-02-26 06:38:23 -0800

    finished landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../styles/landingpage.css'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla impedit
+        officia temporibus obcaecati quasi ratione quae dolor repellendus autem
+        animi. Officia, voluptatem quam quidem repudiandae molestiae quaerat
+        asperiores nemo ex!
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
