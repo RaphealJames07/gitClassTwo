ae790d52a9d38c54f13d39493fe665d7f651744f
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2025-02-26 14:10:30 +0100

    feat

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -9,6 +9,7 @@ const LandingPage = () => {
         officia temporibus obcaecati quasi ratione quae dolor repellendus autem
         animi. Officia, voluptatem quam quidem repudiandae molestiae quaerat
         asperiores nemo ex!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla placeat temporibus tempore fugit earum officia magnam commodi, possimus recusandae aliquam illo voluptates similique tenetur corrupti quidem quaerat consectetur? Deleniti, velit!
       </div>
     </div>
   );
